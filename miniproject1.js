936b463a50c6642d9100cfb2a99652a4affaffec
empero6 <empero6@gmail.com>
2020-03-14 19:15:55 -0400

    Add files via upload
    Recipe pages now have timers :
    - Added two javascript pages for timers

---
+++ miniproject1.js
@@ -0,0 +1,23 @@
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+var timeMe = function () {
+    var x = 25;
+    var mins = 60 * x,
+        display = document.querySelector('#time');
+    startTimer(mins, display);
+};
---
+++ miniproject2.js
@@ -0,0 +1,23 @@
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+var timeMeOne = function () {
+    var x = 5;
+    var mins = 60 * x,
+        display = document.querySelector('#time1');
+    startTimer(mins, display);
+};
